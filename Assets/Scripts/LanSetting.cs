8046ab42b97f18dee34645f1fe6e76266276cae1
unknown <phuocpr1998@gmail.com>
2019-10-04 00:55:05 +0700

    add ui

--- Assets/Scripts/LanSetting.cs
+++ Assets/Scripts/LanSetting.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class LanSetting : MonoBehaviour
 {
+    public GameObject HostUI;
+    public GameObject ClientUI;
+    public GameObject ButtonUI;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnButtonHostClick()
     {
-        
+        HostUI.active = true;
+        ClientUI.active = false;
+        ButtonUI.active = false;
     }
+
+    public void OnButtonClientClick()
+    {
+        HostUI.active = false;
+        ClientUI.active = true;
+        ButtonUI.active = false;
+    }
+
 }
