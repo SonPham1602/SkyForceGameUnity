fb3249481a5956b9126844239a59e843ff0cb9b7
Châu Hoàng Phúc <chauhoangphuc432@gmail.com>
2019-10-07 21:19:53 +0700

    update main menu

---
+++ Assets/Scripts/BGScaller/BGScaller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScaller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        Vector3 tempScale = transform.localScale;
+        float width = sr.bounds.size.x;
+
+        float worldWidth = Camera.main.orthographicSize * 2f;
+        float worldHeight = worldWidth * Screen.height / Screen.width;
+        tempScale.x = worldWidth / width;
+        transform.localScale = tempScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
