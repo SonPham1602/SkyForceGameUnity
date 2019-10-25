5fa22dbc919e8a3c9dcd43f765538c6d6e79d6a5
SonPham1602 <phamngocsonl13.cla@gmail.com>
2019-10-25 20:42:55 +0700

    add homing missile enemy
    Complete: 30%
    Add animation for helicoper

---
+++ SkyForceGameUnity/Assets/Scripts/HomingMissile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomingMissile : MonoBehaviour
+{
+    public Transform target;
+    public float Speed;
+    public float RotateSpeed;
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate() {
+        Vector2 direction = (Vector2)target.position-rb.position;
+        direction.Normalize();
+        float rotateAmount = Vector3.Cross(direction,transform.up).z;
+        rb.angularVelocity = - rotateAmount * RotateSpeed;
+        rb.velocity = transform.up*Speed;
+    }
+}
