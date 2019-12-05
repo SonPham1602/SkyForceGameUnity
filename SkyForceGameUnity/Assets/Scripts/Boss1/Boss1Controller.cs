6a157130d357e4b9fe42c5f8f103f9f56d97b129
SonPham <43876888+SonPham1602@users.noreply.github.com>
2019-12-05 17:12:22 +0700

    Update Start Game

---
+++ SkyForceGameUnity/Assets/Scripts/Boss1/Boss1Controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss1Controller : MonoBehaviour
+{
+    public bool CanMove;
+    public GameObject turret1;
+    public GameObject turret2;
+    public GameObject turret3;
+
+    public GameObject turret4;
+    public GameObject MainTurret;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(CanMove==false)
+        {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
