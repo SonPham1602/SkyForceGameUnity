b4b3e71cbe748e5fe49e188b4ee077f810581cb1
SonPham <43876888+SonPham1602@users.noreply.github.com>
2019-12-07 15:29:27 +0700

    update Pause Game in Gameplay
    Complete: 30%

---
+++ SkyForceGameUnity/Assets/Scripts/KeyboardKeyboardMenu/KeyboardControllerGamePlay.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardControllerGamePlay : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    [SerializeField] GameObject panelPauseGame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+           ResumeGame();
+    }
+
+  
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            GameIsPaused =! GameIsPaused;
+            if(GameIsPaused == true)
+            {
+                PauseGame();
+            }
+            else
+            {
+                ResumeGame();
+            }
+            
+        }
+    }
+    void PauseGame()
+    {
+        Time.timeScale = 0f;
+        panelPauseGame.SetActive(true);
+    }
+    void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        panelPauseGame.SetActive(false);
+    }
+}
