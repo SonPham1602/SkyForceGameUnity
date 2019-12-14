178cc0428a6d631c6cff90f1c07730bed4fc8a34
SonPham1602 <43876888+SonPham1602@users.noreply.github.com>
2019-12-14 15:02:48 +0700

    Add process state of ship

---
+++ SkyForceGameUnity/Assets/Scripts/MenuController/StateUpgradeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateUpgradeController : MonoBehaviour
+{
+
+    public int numberOfState;
+    public GameObject[] scoreOfState;
+    // Start is called before the first frame update
+    void Start()
+    {
+            for (int i = 0; i < numberOfState; i++)
+            {
+                scoreOfState[i].gameObject.GetComponent<UpgradePlayerController>().UpItem();
+            }
+            for (int i = numberOfState; i < 5; i++)
+            {
+                scoreOfState[i].gameObject.GetComponent<UpgradePlayerController>().DownItem();
+            }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void UpState()
+    {
+        if (numberOfState < 5)
+        {
+            numberOfState++;
+            for (int i = 0; i < numberOfState; i++)
+            {
+                scoreOfState[i].gameObject.GetComponent<UpgradePlayerController>().UpItem();
+            }
+            for (int i = numberOfState; i < 5; i++)
+            {
+                scoreOfState[i].gameObject.GetComponent<UpgradePlayerController>().DownItem();
+            }
+        }
+    }
+}
